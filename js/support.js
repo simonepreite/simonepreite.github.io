2731c5c6f62ba4095e302404840b76938ab3b58c
simone.preite <simone-preite_89@hotmail.it>
2017-03-02 20:20:02 +0100

    aggiunta sezione editorial

--- js/support.js
+++ js/support.js
@@ -6,4 +6,9 @@ $(document).ready(function(){
         nav_background: "black"
   }
   $(".custom-navbar").navbar(custom);
+  $('ul.nav li.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+  });
 });
